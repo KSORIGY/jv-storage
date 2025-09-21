6c3ff01e0925452ff862328e907c0020d00c2763
DenysHirniak <denforever777@gmail.com>
2025-09-21 14:59:13 +0300

    codestyle fixes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
@@ -11,6 +10,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private K[] keys = (K[]) new Object[MAX_ELEMENTS_IN_STORAGE];
     private V[] values = (V[]) new Object[MAX_ELEMENTS_IN_STORAGE];
     private int storageSize = INITIAL_SIZE_OF_STORAGE;
+
     @Override
     public void put(K key, V value) throws RuntimeException {
         for (int i = 0; i < storageSize; i++) {
@@ -20,7 +20,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             }
         }
 
-        if(storageSize == MAX_ELEMENTS_IN_STORAGE) {
+        if (storageSize == MAX_ELEMENTS_IN_STORAGE) {
             throw new RuntimeException("Storage is full");
         } else {
             keys[storageSize] = key;
